564bef5fb1d42a06d759a896a99fa2d58bb74946
Oğuz Han Turan <turan.oguzhan@hotmail.com>
2024-08-09 08:52:41 +0300

    password encoder was disabled

--- src/main/java/org/ouz/roombahoover/config/SecurityConfig.java
+++ src/main/java/org/ouz/roombahoover/config/SecurityConfig.java
@@ -8,13 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
 
@@ -30,7 +28,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .anyRequest().hasRole("USER")
+                        .requestMatchers("api/robot/**").hasRole("USER")
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(httpBasic -> httpBasic
                         .realmName("Hoover API")
@@ -50,8 +49,10 @@ public class SecurityConfig {
         );
     }
 
+    /*
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+    */
 }
